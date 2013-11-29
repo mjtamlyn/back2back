5991d9254e53c2a9b2c2ed2b29da837f95dbc939
Marc Tamlyn <marc.tamlyn@gmail.com>
2013-11-29 11:07:00 +0000

    Expand tests, but currently ignore max entry number.

--- back2back/structure.py
+++ back2back/structure.py
@@ -2,6 +2,8 @@ from .models import Entry
 
 
 class BaseCategory(object):
+    max_entries = 24
+
     def create_entry(self, name, agb_number=''):
         Entry.objects.create(category=self.slug, name=name, agb_number=agb_number)
 
--- back2back/tests/test_entry.py
+++ back2back/tests/test_entry.py
@@ -1,3 +1,5 @@
+import unittest
+
 from django.test import TestCase
 
 from back2back.models import Entry
@@ -10,3 +12,15 @@ class TestCreatingEntry(TestCase):
         category.create_entry('Larry Godfrey')
         self.assertEqual(Entry.objects.count(), 1)
         self.assertIsInstance(Entry.objects.get().get_category(), GentsRecurve)
+
+    def test_with_agb_number(self):
+        category = GentsRecurve()
+        category.create_entry('Marc Tamlyn', '1039623')
+        self.assertEqual(Entry.objects.count(), 1)
+        self.assertEqual(Entry.objects.get().agb_number, '1039623')
+
+    @unittest.skip
+    def test_limit(self):
+        category = GentsRecurve()
+        for i in range(category.max_entries):
+            category.create_entry('Archer %s' % i)
