3e24c93053a603aaa3faed9f09cfbf7cfe3aea7e
Marc Tamlyn <marc.tamlyn@gmail.com>
2013-12-10 22:43:22 +0000

    Slight admin improvement.

--- back2back/admin.py
+++ back2back/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 from .models import Entry, Score
 
 
-admin.site.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_filter = ('category',)
+
+
+admin.site.register(Entry, EntryAdmin)
 admin.site.register(Score)
