f996755665c9e55af5139a473b859aa0eb507515
Marc Tamlyn <marc.tamlyn@gmail.com>
2015-12-02 19:35:03 +0000

    Remove MediaCling as there isn't any.

--- back2back/wsgi.py
+++ back2back/wsgi.py
@@ -2,6 +2,6 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "back2back.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling, MediaCling
+from dj_static import Cling
 
-application = Cling(MediaCling(get_wsgi_application()))
+application = Cling(get_wsgi_application())
